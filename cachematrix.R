56366572c863e157db43addbf2863b1af9ace236
jaskhu <jaskhu@gmail.com>
2019-05-26 18:48:12 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,33 @@
 ## functions do
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+# R code to compute inverse of matrix by caching the data
+makeCacheMatrix <- function(gettingdat = matrix()) {
+  lime <- NULL
+  set <- function(y) {
+    gettingdat <<- y			#stores data
+    lime <<- NULL
+  }
+  get <- function() gettingdat
+  setinverse <- function(inverse) lime <<- inverse	#stores inversion data
+  getinverse <- function() lime
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(gettingdat, ...) {
+  lime <- gettingdat$getinverse()	#fetch already evaluated data
+  if (!is.null(lime)) {
+    message("getting the cached data from prev")		#checkif already evaluated
+    return(lime)	#return inversion from cached data
+  }
+  
+  data <- gettingdat$get()	#get the input data if not already evaluated
+  lime <- solve(data, ...)		#evaluate Matrix Inversion
+  gettingdat$setinverse(lime)	
+  lime				#return the inversion
 }
+#Create a Matrix as A and input as A1 <- makeCacheMatrix(A) and then run cacheSolve(A1) to get your inversion matrix from cached data
+
